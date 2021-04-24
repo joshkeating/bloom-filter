e1f5afb7de606ce44ac27287b1a68b42ac42316e
Sam Gehman <sgehman@cs.uw.edu>
2021-04-24 16:56:39 -0700

    First pass as bitset

--- src/bitset.rs
+++ src/bitset.rs
@@ -4,27 +4,45 @@ pub struct BitSet {
 }
 
 impl BitSet {
-    pub fn new(size: u32) {
-        let size: usize = usize(ceil(f32(size) / 8));
-        let data: Vec<u8, size> = vec![0; size];
-        Bitset { size, data }
+    pub fn new(size: u32) -> BitSet {
+        let num_bytes: u32 = (size as f32 / 8.0).ceil() as u32;
+        BitSet { size: num_bytes, data: vec![0; size as usize] }
     }
 
-    fn index
-
-    pub fn set(&mut self, index: u32) -> Result<(), E> {
-        if index >= self.size {
-            Ok(())
+    fn get_position(&self, i: u32) -> Option<(u32, u32)> {
+        if i >= self.size {
+            None
         } else {
-            Err("index out of bounds")
+            let index = i / 8;
+            let offset = i % 8;
+            Some((index, offset))
         }
     }
 
-    pub fn get(&self, index: u32) -> Some<Bool> {
+    pub fn set(&mut self, index: u32) -> Result<(), String> {
+        if let Some((index, offset)) = self.get_position(index) {
+            self.data[index as usize] |= 1 << offset;
+            Ok(())
+        } else {
+            Err(format!("index out of bounds: {i}", i = index))
+        }
+    }
+
+    pub fn get(&self, index: u32) -> Option<bool> {
         if index >= self.size {
             None
         } else {
-            Some()
+            Some(false)
         }
     }
+}
+
+#[test]
+fn test1() {
+    assert!(true);
+}
+
+#[test]
+fn test2() {
+    assert!(false);
 }
