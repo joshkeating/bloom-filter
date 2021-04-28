8b6440c4b3eccc541ec3480f867f20901bbd43f8
Sam Gehman <sgehman@cs.uw.edu>
2021-04-28 16:10:16 -0700

    Add test

--- src/bloom.rs
+++ src/bloom.rs
@@ -6,11 +6,12 @@
   https://llimllib.github.io/bloomfilter-tutorial/
 */
 
-use crate::bitset::BitSet;
-use std::hash::{Hash, BuildHasher, Hasher};
-use std::collections::hash_map::{RandomState, DefaultHasher};
-use std::slice::Iter;
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hash, Hasher};
 use std::iter::Map;
+use std::slice::Iter;
+
+use crate::bitset::BitSet;
 
 pub struct Bloom {
     bitset: BitSet,
@@ -26,24 +27,30 @@ impl Bloom {
         Bloom { bitset, random_states }
     }
 
-    pub fn insert<T: Hash>(&mut self, x: T) {
-        for i in self.get_indices(x) {
+    pub fn insert<T: Hash>(&mut self, value: T) {
+        for i in self.get_indices(value) {
             self.bitset.set(i).unwrap();
         }
     }
 
-    pub fn contains<T: Hash>(&self, x: T) {}
+    pub fn contains<T: Hash>(&self, value: T) {}
 
-    fn get_indices<T: Hash>(&self, x: T) -> Vec<usize> {
+    fn get_indices<T: Hash>(&self, value: T) -> Vec<usize> {
         self.random_states.iter().map(|random_state| {
-            println!("New hash!");
             let mut hasher = random_state.build_hasher();
-            x.hash(&mut hasher);
+            value.hash(&mut hasher);
             (hasher.finish() % self.bitset.size as u64) as usize
         }).collect::<Vec<usize>>()
     }
 }
 
 #[test]
-fn test_fmt() {
+fn test_insert() {
+    let x = "hello there";
+    let y = "goodbye";
+
+    let mut bloom = Bloom::new(32, 1);
+    bloom.insert(x);
+    bloom.insert(x);
+    bloom.insert(y);
 }
